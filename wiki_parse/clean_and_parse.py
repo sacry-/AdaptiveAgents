82a4210a84aab5204bf46596870070318a6ff480
sacry- <nitsche25@gmail.com>
2014-10-27 13:51:10 +0100

    added some basic tokenizing etc

--- wiki_parse/clean_and_parse.py
+++ wiki_parse/clean_and_parse.py
@@ -1,23 +1,64 @@
-from nltk import clean_html
+from nltk import clean_html, word_tokenize, data
+
 from dewiki.parser import Parser # https://github.com/daddyd/dewiki.git
 from wiki_api import fetch_articles_by_titles
 from io_utils import read, save
+import re
+import time
 
-# String -> String
+def remove_special(s):
+  buf = []
+  c = 0
+  for char in s:
+    if char == '{':
+      c += 1
+      continue
+    elif char == '}':
+      c -=1
+      continue
+    elif c == 0:
+      buf.append(char)
+  return "".join(buf)
+
 def remove_markup(s):
   return Parser().parse_string(s)
 
 def remove_html(s):
   return clean_html(s)
 
-def test():
-  article = fetch_articles_by_titles(["biology"], 1)
-  save("biology.txt", article[0]["content"])
-  t = read("biology.txt")
-  t1 = remove_markup(t)
-  save("biology_1.txt", t1)
-  t2 = remove_html(t1)
-  save("biology_2.txt", t2)
-  print "Done!"
+def tag(sentence):
+  return str(word_tokenize(sentence))
 
-test()
+def sentence_tokenize(s):
+  sent_detector = data.load('tokenizers/punkt/english.pickle')
+  sentences = []
+  for sentence in sent_detector.tokenize(s.strip()):
+    if sentence.find("See also") != -1:
+      return "\n--------\n".join(sentences)
+    sentences.append(tag(sentence))
+  return "\n--------\n".join(sentences)
+
+# single_step_test("biology_2.txt", "biology_nested.txt", remove_special)
+def single_step_test(fn_read, fn_write, f):
+  save(fn_write, f(read(fn_read)))
+
+def test():
+  #article = fetch_articles_by_titles(["biology"], 1)
+  #save("biology.txt", article[0]["content"])
+  base = "test_data/"
+  t = read("%sbiology.txt" % base)
+  t1 = remove_special(t)
+  save("%sbiology_1.txt" % base, t1)
+  t2 = remove_markup(t1)
+  save("%sbiology_2.txt" % base, t2)
+  t3 = remove_html(t2)
+  save("%sbiology_3.txt" % base, t3)
+  t4 = sentence_tokenize(t3)
+  save("%sbiology_4.txt" % base, str(t4))
+
+t = time.clock()
+for _ in range(0, 1):
+  test()
+t1 = time.clock()
+print "%s" % (t1 - t)
+
