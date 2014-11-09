d060bf196b6dffbec463bf0ba052febb332ed873
sacry- <nitsche25@gmail.com>
2014-11-09 11:59:52 +0100

    removed unnecessary calls, all 'test' files must begin with sample_ in the file name

--- love_the_data/main.py
+++ love_the_data/main.py
@@ -23,7 +23,7 @@ save_json(h, "sample_statistics")
 
 t1 = time.clock()
 c = 0
-lexi = 0
+
 biology_words = set([])
 biology_average_word_size = 0
 
@@ -32,22 +32,16 @@ try:
   for articles in es.generator_scroll("biology", "title", 25):
     for source in map(lambda a: a["_source"], articles):
       h = statistic_hash(source["content"])
-      title = source["title"]
       new_words = set(h["lexicon"]["freq_dist"].keys())
       biology_average_word_size += h["avg_word_size"]
       s3 = biology_words.union(new_words)
-      try:
-        diff = (len(new_words) / len(biology_words)) * 100
-      except:
-        diff = 0
       biology_words = s3
       c += 1
-      print ("%s. %s - lex_div: %.2f" % (c, title, h["lex_div"]))
-    print "average word size: %s" % (biology_average_word_size / c)
+    print "%s articles processed, leixcon has %s words" % (c, len(biology_words))
 except:
   pass
 
-save_json({"avg_word_size" : biology_average_word_size, "sample_biology_words" : list(biology_words)}, "biology_words")
+save_json({"avg_word_size" : biology_average_word_size / c, "sample_biology_words" : list(biology_words)}, "biology_words")
 print "time needed: %s for articles: %s" % (time.clock() - t1, c)
 
 
