c112651ec8d45362e6d719117e4d11542538db37
sacry- <nitsche25@gmail.com>
2014-11-09 12:27:57 +0100

    small refactors on main

--- love_the_data/main.py
+++ love_the_data/main.py
@@ -11,39 +11,41 @@ from io_utils import save_json
 from statistics import statistic_hash
 
 
+def single_test(es):
+  titles = ["biology", "biologist", "biological_ornament", "birth", "cell_population_data",
+            "brian_dale", "dependence_receptor", "despeciation", "biologist", "biology"]
+  articles = list(es.get_multiple_articles("biology", "title", titles))
+  h = statistic_hash(articles[0])
+  save_json("love_the_data", "sample_statistics", h)
+
+
+def multiple_test(es):
+  t1 = time.clock()
+  c = 0
+
+  biology_words = set([])
+  biology_average_word_size = 0
+
+  # time needed: 46.898816 for articles: 1000 forall ~> 2hours
+  try:
+    for articles in es.generator_scroll("biology", "title", 25):
+      for source in map(lambda a: a["_source"], articles):
+        h = statistic_hash(source["content"])
+        new_words = set(h["lexicon"]["freq_dist"].keys())
+        biology_average_word_size += h["avg_word_size"]
+        s3 = biology_words.union(new_words)
+        biology_words = s3
+        c += 1
+      print "%s articles processed, lexicon has %s words" % (c, len(biology_words))
+  except:
+    pass
+
+  h = {"avg_word_size" : biology_average_word_size / c, "biology_words" : list(biology_words)}
+  # save_json("love_the_data", "biology_words", h)
+  print "time needed: %s for articles: %s" % (time.clock() - t1, c)
+
+
 es = Elastic()
-
-'''
-titles = ["biology", "biologist", "biological_ornament", "birth", "cell_population_data",
-          "brian_dale", "dependence_receptor", "despeciation", "biologist", "biology"]
-articles = list(es.get_multiple_articles("biology", "title", titles))
-h = statistic_hash(articles[0])
-save_json("love_the_data", "sample_statistics", h)
-'''
-
-t1 = time.clock()
-c = 0
-
-biology_words = set([])
-biology_average_word_size = 0
-
-# time needed: 46.898816 for articles: 1000 forall ~> 2hours
-try:
-  for articles in es.generator_scroll("biology", "title", 25):
-    for source in map(lambda a: a["_source"], articles):
-      h = statistic_hash(source["content"])
-      new_words = set(h["lexicon"]["freq_dist"].keys())
-      biology_average_word_size += h["avg_word_size"]
-      s3 = biology_words.union(new_words)
-      biology_words = s3
-      c += 1
-    print "%s articles processed, lexicon has %s words" % (c, len(biology_words))
-except:
-  pass
-
-h = {"avg_word_size" : biology_average_word_size / c, "biology_words" : list(biology_words)}
-save_json("love_the_data", "biology_words", h)
-print "time needed: %s for articles: %s" % (time.clock() - t1, c)
-
+single_test(es)
 
 
