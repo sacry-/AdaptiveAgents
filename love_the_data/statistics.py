2600f63205b537557a38443060ffe431f9e29234
GollyTicker <golly.ticker@gmail.com>
2014-11-12 23:22:05 +0100

    added stddev and shifted both to lemmas level

--- love_the_data/statistics.py
+++ love_the_data/statistics.py
@@ -8,14 +8,14 @@ from syntax import remove_noise
 from syntax import lemmatize, stemmatize
 from syntax import word_complexity
 from syntax import pos_tag, lemmatize_by_pos_tags
+from math import sqrt
+import random
 
 def statistic_hash(text, h={}):
   # Before clean up
   tokens = wiki_tokenize(text)
-  h["lex_div"] = lexical_diversity(tokens)
   # After clean up
   tokens_rn = remove_noise(tokens)
-  h["avg_word_size"] = average_word_size(tokens_rn)
   lemmas = list(stemmatize(tokens_rn))
   h["lemmas"] = lemmas_hash(lemmas)
   return { "stats" : h }
@@ -30,30 +30,41 @@ def new_statistic_hash(text, h={}):
   return { "stats" : h }
 
 def lemmas_hash(lemmas):
-  freqs, avg_lemma_complexity = freq_dict(frequency([word.lower() for word in lemmas]))
+  freqs, avg_lc, stddev_lc = freq_dict(frequency([word.lower() for word in lemmas]))
   words = freqs.keys()
+  avg_ws, stddev_ws = avg_and_stddev_by(len, words, 0)
   return { 
     "freq_dist" : freqs,
     "lex_div" : lexical_diversity(lemmas),
     "size" : len(words),
-    "avg_lemma_complexity": avg_lemma_complexity,
-    "avg_word_size" : average_word_size(words)
+    "avg_lemma_complexity": avg_lc,
+    "dev_lemma_complexity": stddev_lc,
+    "avg_word_size" : avg_ws,
+    "dev_word_size" : stddev_ws
   }
-
-def average_lemma_complexity(accumulated_complexity, lemma_size):
-  try:
-    return accumulated_complexity / lemma_size
-  except:
-    return 0
-
-def average_word_size(words):
-  return average_by(len, words, 0)
 
 def average_by(f, words, default):
   try:
     return reduce(lambda a,x: a + f(x), words, 0) / len(words)
   except:
     return default
+
+def avg_and_stddev_by(f, ls, default): # http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Incremental_algorithm
+    n = 0
+    mean = default
+    m2 = 0
+    for x in ls:
+        n += 1
+        y = f(x)
+        delta = y - mean
+        mean += delta/n
+        m2 += delta*(y - mean)
+ 
+    if (n < 2):
+        return mean, 0
+ 
+    stddev = sqrt(m2/(n - 1))
+    return mean, stddev
 
 def lexical_diversity(tokens):
   try:
@@ -68,11 +79,8 @@ def freq_dict(fdist):
   d = {}
   acc_word_complexity = 0
   for lemma, count in fdist.iteritems():
-    complexity = word_complexity(lemma)
-    d[lemma] = (count, complexity)
-    acc_word_complexity += complexity
-  return (d, 
-    average_lemma_complexity(acc_word_complexity, len(fdist.keys()))
-  )
+    d[lemma] = (count, word_complexity(lemma))
+  avg, stddev = avg_and_stddev_by(lambda x:x[1], d.values(), 0)
+  return (d, avg, stddev)
 
 
