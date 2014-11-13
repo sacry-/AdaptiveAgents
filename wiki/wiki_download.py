987ab24b26db17b0ebcae9397f2863cefe46ea7d
sacry- <nitsche25@gmail.com>
2014-11-13 23:39:00 +0100

    added rediss to all wiki api calls

--- wiki/wiki_download.py
+++ wiki/wiki_download.py
@@ -7,7 +7,7 @@ import time
 from wiki_api import categories_by_depth, fetch_articles_by_titles
 from io_utils import load_json, save_json, flatten_hash, create_file_name
 from category_knowledge import saved_titles
-from elastic import Elastic
+from rediss import Rediss
 
 def download_wikipedia_titles(categories, depth):
   persisted_titles = persisted_titles = saved_titles(depth)
@@ -26,30 +26,26 @@ def print_download_status(titles_size, done, acc):
   needed = ((acc / done) * articles_to_go) / 60
   print "%s articles and %s mins to go" % (articles_to_go, needed)
 
-def fetch_articles_and_add_to_elastic_search(es, titles, _index, _doc_type, _group_factor=10):
+def fetch_articles_and_add_to_elastic_search(rss, titles, category, group_factor=100):
   group_factor, done = _group_factor, 0
   grouped_titles = [titles[i:i+group_factor] for i in range(0, len(titles), group_factor)]
   last, acc = 0, 0
   for sub_titles in grouped_titles:
     before = time.time()
     all_articles = fetch_articles_by_titles(title_list=sub_titles, limit=len(sub_titles))
-    es.add_multiple_articles(_index, _doc_type, all_articles)
+    rss.add_articles_and_pos_tags(category, all_articles)
     end = time.time() - before
     acc += end
     done += group_factor
     print_download_status(len(titles), done, acc)
 
 def download_article_and_add_to_elastic_search(categories, depth):
-  elastic = Elastic("localhost", 9200)
+  rss = Rediss()
   for category in categories:
     file_name = create_file_name(category, depth)
     titles = list(flatten_hash(load_json("categories", file_name)))
     print "fetched %s titles from %s..." % (len(titles), file_name)
-    index, doc_type = category.lower(), "title"
-    elastic.dummy(index)
-    titles = elastic.all_unpersisted_titles(index, doc_type, titles)
-    print "articles to go: %s" % len(titles)
-    fetch_articles_and_add_to_elastic_search(elastic, titles, index, doc_type)
+    fetch_articles_and_add_to_elastic_search(rss, titles, category.lower())
     print "done downloading for index=%s and doc_type=%s" % (index, doc_type)
   print "all done!"
 
