c97bdf10410ef337a6443f5b80ec9fbe2fecd8f2
GollyTicker <golly.ticker@gmail.com>
2014-11-19 18:53:48 +0100

    added weight and feature vector. working but slow. needs multi-get variants of tf, idf and weight.

--- love_the_data/statistics.py
+++ love_the_data/statistics.py
@@ -11,18 +11,32 @@ from syntax import pos_tag, stem_with_pos_tags
 from syntax import LETTER_FREQ
 from math import sqrt
 import random
+import time
 
+# if this is set to true, then call Frequencies(..., rfeature) with rfeature as an extra argument.
+# this makes calls to tf and idf much faster and there is no overhead in loading at the beginning.
+REDIS_HAS_IDFS = True
 
 class Frequencies():
 
-  def __init__(self, rpos, category):
+  def __init__(self, rpos, category, rfeature=None):
+    if rfeature:
+        self.rfeature = rfeature
     self.titles = map(lambda x: rpos.real_title(x), rpos.keys("%s*" % category))
+    self.rpos = rpos
     # freqs = {"biologist": Frequency(...), "despiciation": Frequency(...), ...}
-    self.freqs = dict((title, Frequency(ptags)) for title, ptags in zip(self.titles, rpos.values_by_titles(category, self.titles)))
+    if not REDIS_HAS_IDFS:
+        self.freqs = dict((title, Frequency(ptags)) for title, ptags in zip(self.titles, rpos.values_by_titles(category, self.titles)))
+    else:
+        self.freqs = {}
     self.num_of_docs = len(self.titles)
     self.cache = {} # saches calls of idf(t)
+    self.cat = category
 
+  # Term -> Float
   def idf(self, t):
+    if REDIS_HAS_IDFS:
+        return self.rfeature.value_by_title(self.cat, t) or 0
     if self.cache.has_key(t):
       return self.cache[t]
     n = reduce(lambda acc, freq: acc + freq.idf(t), self.freqs.values(), 0)
@@ -34,11 +48,31 @@ class Frequencies():
       return 0
   
   # term frequency of term t in document d. if document d doesn't exist, it returns 0
+  # Term -> Title -> Int
   def tf(self, t, title):
+    if REDIS_HAS_IDFS:
+        self.freqs[title] = Frequency(self.rpos.value_by_title(self.cat, title))
     if not self.freqs.has_key(title):
         return 0
-    b = self.freqs[title]
-    return b.tf(t)
+    return self.freqs[title].tf(t)
+    
+  # Term -> Title -> Float
+  def w(self, t, d):
+    return self.tf(t,d) * self.idf(t)
+  
+  # Title ( -> Int) -> [(Term,Float)]
+  def feature_vector(self, d, maxLen=25):
+    self.tf("dummy",d) # force self.freqs[d] to be loaded
+    words = self.freqs[d].words()
+    print "[2] %s len: %s" % (diff(), self.freqs[d].size())
+    
+    weighted_vector = map(lambda t: (t, self.w(t,d)) , words)
+    # this is the bottle neck. this is why, calculating the feature vector takes so long.
+    # it take ~15 seconds for a signel article with ~954 words.
+    # this could be optimized by implementing multi get for tf, idf and weight. this will make this faster definitely.
+    
+    fvector = sorted(weighted_vector, key=lambda x:-x[1]) # sort by weight descending
+    return fvector[0:maxLen]
 
 class Frequency():
 
