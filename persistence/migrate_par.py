d839d436a1459c95e4476a457d45abc4085d8ab9
GollyTicker <golly.ticker@gmail.com>
2014-11-19 15:10:26 +0100

    to migrate use: python migrate_par.py -cat "biology"

--- persistence/migrate_par.py
+++ persistence/migrate_par.py
@@ -9,16 +9,17 @@ sys.path.insert(0, p)
 from statistics import Frequencies
 
 
-# brew/apt-get? install redis
-# sudo pip install redis-py
-# sudo pip install redisdl
-# DONE! :-)
+# USAGE:
+# Open three terminals
+# [1]: python migrate_par.py -cat "physics"
+# [2]: python migrate_par.py -cat "chemistry"
+# [3]: python migrate_par.py -cat "biology"
+# each process will occupy a core. close other memory hungy applications, if you don't have more than 4 cores...
 
 rpos = RPos()
 rfeature = RFeature()
 
 n = 0
-categories = ["biology", "physics", "chemistry"]
 t__ = time.clock()
 def diff():
     return "%s" % ((time.clock() - t__)) 
@@ -28,21 +29,23 @@ def do_category(category):
     print "[1] %s %s - Calculating idfs for category %s" % (diff(), category, category)
     
     frequencies = Frequencies(rpos, category)
-    
-    print "[1] %s %s - idfs for %s articles calculated" % (diff(), category, len(frequencies.titles))
+    numArticles = len(frequencies.titles)
+    print "[1] %s %s - idfs for %s articles calculated" % (diff(), category, numArticles)
     print "[1] %s %s - Inserting idfs into %s" % (diff(), category, rfeature)
     
     curr = pre = len(list(rfeature.keys(category + "*")))
     i = 1
+    j = 0
     pipe = rfeature.rs.pipeline(transaction=False)
     for f in frequencies.freqs.values():
+        j += 1
         for t in f.words():
             i += 1
             if i % 5000 == 0:
                 pipe.execute() # Section Pipeline: https://pypi.python.org/pypi/redis/
                 pipe = rfeature.rs.pipeline(transaction=False)
                 curr = len(list(rfeature.keys(category + "*")))
-                print "[2] %s %s - i = %s" % (diff(),category,  i)
+                print "[2] %s %s - processed %s/%s articles, i = %s" % (diff(), category, j, numArticles, i)
                 print "[2] %s %s - added %s terms into RFeature, %s total" % (diff(), category, curr - pre, curr)
                 pre = curr
             _idf = frequencies.idf(t)
@@ -51,10 +54,16 @@ def do_category(category):
     print "[2] %s %s - Finished all %s terms in %s" % (diff(), category, len(list(rfeature.keys(category + "*"))))
     return "fin"
 
+import argparse
+parser = argparse.ArgumentParser(description="Migrate a category")
+parser.add_argument('-cat', action='store', metavar="CATEGORY", help="Specify the name of the category to process.")
+args = parser.parse_args()
 
+if not args.cat:
+    print "Please specify category. eg. \"biology\""
 
 print "[0] %s - Started migrate script for inverse document frequencies" % diff()
 
-futures = map(do_category, categories)
+do_category(args.cat)
 
 print "[0] Finished migration: %s terms over %s categories" % (diff(), n)
